cea8aed39c86263a34512f2543a90475b91f5d93
trydex <tryddx@gmail.com>
2023-04-28 07:04:45 +0700

    remove redundant code

--- src/CodeRunner.Worker/Program.cs
+++ src/CodeRunner.Worker/Program.cs
@@ -8,14 +8,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var host = CreateDefaultBuilder().Build();
-        
-        using var serviceScope = host.Services.CreateScope();
-        var provider = serviceScope.ServiceProvider;
-        var workerInstance = provider.GetRequiredService<Worker>();
-        workerInstance.DoWork();
-
-        host.Run();
+        CreateDefaultBuilder().Build().Run();
     }
 
     static IHostBuilder CreateDefaultBuilder()
@@ -25,55 +18,25 @@ class Program
             {
                 app.AddJsonFile("appsettings.json");
             })
-            .ConfigureServices(services =>
-            {
-                ConfigureQuartz(services);
-                services.AddSingleton<Worker>();
-            });
+            .ConfigureServices(ConfigureQuartz);
     }
 
     static void ConfigureQuartz(IServiceCollection services)
     {
         services.AddQuartz(q =>
         {
-            // Just use the name of your job that you created in the Jobs folder.
             var jobKey = new JobKey(nameof(CodeExecutionJob));
             q.AddJob<CodeExecutionJob>(opts => opts.WithIdentity(jobKey));
 
             q.AddTrigger(opts => opts
                 .ForJob(jobKey)
                 .WithIdentity($"{nameof(CodeExecutionJob)}-trigger")
-                //This Cron interval can be described as "run every minute" (when second is zero)
                 .WithSimpleSchedule(x => x
                     .WithIntervalInSeconds(10)
                     .RepeatForever())
             );
         });
+
         services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
-    }
-}
-
-internal class Worker
-{
-    private readonly IConfiguration configuration;
-
-    public Worker(IConfiguration configuration)
-    {
-        this.configuration = configuration;
-    }
-
-    public void DoWork()
-    {
-        var keyValuePairs = configuration.AsEnumerable().ToList();
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("==============================================");
-        Console.WriteLine("Configurations...");
-        Console.WriteLine("==============================================");
-        foreach (var pair in keyValuePairs)
-        {
-            Console.WriteLine($"{pair.Key} - {pair.Value}");
-        }
-        Console.WriteLine("==============================================");
-        Console.ResetColor();
     }
 }
