c55dac7faeac39aef0677cf9e2468fd3d5d07094
trydex <tryddx@gmail.com>
2023-04-30 11:57:26 +0700

    copying ace editor themes

--- src/CodeRunner.UI/src/main.js
+++ src/CodeRunner.UI/src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ace from 'ace-builds/src-noconflict/ace'
 
 import './assets/main.css'
-
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
 
@@ -19,6 +19,8 @@ import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
 library.add(faPlay, faTrash, faCopy, faAngleLeft, faAngleRight)
+
+ace.config.set("basePath", "./node_modules/ace-builds/src-noconflict");
 
 const app = createApp(App)
 
--- src/CodeRunner.UI/vite.config.js
+++ src/CodeRunner.UI/vite.config.js
@@ -1,11 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import copy from 'rollup-plugin-copy';
 import vue from '@vitejs/plugin-vue'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    copy({
+      targets: [
+        {
+          src: './node_modules/ace-builds/src-min-noconflict/*',
+          dest: './dist/node_modules/ace-builds/src-noconflict'
+        }
+      ],
+      verbose: true,
+      hook: 'writeBundle'
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
