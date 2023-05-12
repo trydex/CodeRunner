1495dc42866d6633dc516e80947442d181ba2c64
trydex <tryddx@gmail.com>
2023-05-12 22:57:11 +0700

    added go templates

--- src/CodeRunner.UI/src/templates.js
+++ src/CodeRunner.UI/src/templates.js
@@ -27,7 +27,7 @@ for(var i = 0; i < 10; i++)
 
     "golang": [
     {
-        processCount: 1,
+        processCount: 2,
         code: `
 package main
 
@@ -38,6 +38,77 @@ import (
 func main() {
     fmt.Printf("Hello World")
 }`
+    }, 
+    {
+        processCount: 1,
+        code: `
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func main() {
+
+    wg := &sync.WaitGroup{}
+
+    for i := 0; i < 10; i++ {
+        i := i
+        go func() {
+            wg.Add(1)
+            defer wg.Done()
+
+            fmt.Println(i)
+        }()
+    }
+
+    wg.Wait()
+}`
     },
+    {
+        processCount: 1,
+        code: `
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func main() {
+
+    wg := &sync.WaitGroup{}
+    ch := make(chan int)
+
+    wg.Add(2)
+    go func() {
+        defer wg.Done()
+        producer(ch)
+    }()
+
+    go func() {
+        defer wg.Done()
+        consumer(ch)
+    }()
+
+    wg.Wait()
+}
+
+func producer(ch chan<- int) {
+    defer close(ch)
+
+    for i := 0; i < 10; i++ {
+        ch <- i
+        fmt.Printf("Produce - %v\\n", i)
+    }
+}
+
+func consumer(ch <-chan int) {
+    for v := range ch {
+        fmt.Printf("Consume - %v\\n", v)
+    }
+}`
+    }
 ]
 }
