21151d67330a307e6128cf53d55869aaf01ac041
trydex <tryddx@gmail.com>
2023-05-12 23:14:21 +0700

    change language selector style

--- src/CodeRunner.UI/src/components/CodeRunner.vue
+++ src/CodeRunner.UI/src/components/CodeRunner.vue
@@ -297,4 +297,29 @@ export default {
   margin-left: 10px;
   margin-right: 10px;
 }
+
+select::-ms-expand {
+  display: none;
+}
+select {
+  display: inline-block;
+  box-sizing: border-box;
+  padding: 0.5em 2em 0.5em 0.5em;
+  border: 0px #36393d solid;
+  border-radius: 4px;
+  line-height: inherit;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  -ms-appearance: none;
+  appearance: none;
+  background-repeat: no-repeat;
+  background-image: linear-gradient(45deg, transparent 50%, currentColor 50%), linear-gradient(135deg, currentColor 50%, transparent 50%);
+  background-position: right 15px top 1em, right 10px top 1em;
+  background-size: 5px 5px, 5px 5px;
+  font-size: 12px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #eeeaea;
+  background-color: #36393d;
+}
 </style>
